72a74bbbfc6fabd9c60710d41d04fb868c470afd
diggzhang <diggzhang@gmail.com>
2016-01-13 14:53:43 +0800

    koa kickstart

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # koala
+
+the complete restful koajs app, use for onions event API request server now 
---
+++ index.js
@@ -0,0 +1,9 @@
+var koa = require('koa');
+var app = koa();
+var port = process.argv[2];
+
+app.listen(port);
+
+app.use(function *() {
+    this.body = 'hello'
+});
